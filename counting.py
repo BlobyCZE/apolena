3339880cf77583f60203861e4e32d3e4ccd0c6f1
BlobyCZ <136823612+BlobyCZE@users.noreply.github.com>
2024-07-01 19:08:24 +0200

    možná update | counting.py

--- counting.py
+++ counting.py
@@ -1,32 +1,31 @@
-# Plus
+# +
 def add(num1, num2):
     return num1 + num2
-# Mínus
+# -
 def subtract(num1 , num2):
   return num1 - num2
-# Násobení
+# *
 def multiply(num1, num2):
    return num1 * num2
-# Dělení
+# /
 def divide(num1, num2):
    return num1 / num2
-# Dělení je multiply docela, insane nejvíc
+# //
 def integer_divide(num1 , num2):
    return num1 // num2
-# Dělení modulo
+# %
 def modulo(num1, num2):
    return num1 % num2
 
 # Nějaký příklad
-num1 = 10
-num2 = 4
+num1 = float(input('ZADEJ SEM SVŮJ PŘÍKLAD:'))
+num2 = float(input('ZADEJ SEM SVŮJ PŘÍKLAD:'))
 
 # Printy
-print("Celý plus", add(num1, num2)) # Plus jsou jako třeba num1+num2
-print("Mínus:", subtract(num1, num2)) # Mínus = num1-num2
-print("Násobení:", multiply(num1, num2))
-print("Dělení:", divide(num1, num2))
-print("Dělení je ještě víc multiply:", integer_divide(num1, num2))
-print("Dělení je moje modulo!", modulo(num1, num2))
-
+print("+:", add(num1, num2)) # Plus jsou jako třeba num1+num2
+print("-:", subtract(num1, num2)) # Mínus = num1-num2
+print("*:", multiply(num1, num2))
+print("/:", divide(num1, num2))
+print("//:", integer_divide(num1, num2))
+print("%", modulo(num1, num2))
 print("Vytvořil s láskou BlobyCZ | https://github.com/blobycze/pocitadlo")
